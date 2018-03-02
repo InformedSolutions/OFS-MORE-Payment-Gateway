e2e8a67148fa6614904447d8db3294620aec49bc
Cruddas <james.cruddas@informed.com>
2018-03-02 14:28:21 +0000

    build.py: Add django toolbar middleware to settings

---
+++ payment_gateway/settings/build.py
@@ -0,0 +1,25 @@
+from .base import *
+
+WORLDPAY_API_KEY = 'T_S_affb6e01-fd4e-42e4-bed6-5cc45e38ed57'
+
+DEBUG = True
+
+TEST_MODE = True
+
+ALLOWED_HOSTS = ['*']
+PUBLIC_APPLICATION_URL = 'http://localhost:8000/payment-service'
+INTERNAL_IPS = "127.0.0.1"
+
+DEV_APPS = [
+  'debug_toolbar'
+]
+
+MIDDLEWARE_DEV = [
+  'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
+MIDDLEWARE = MIDDLEWARE + MIDDLEWARE_DEV
+INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + DEV_APPS + PROJECT_APPS
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '-m*c=fbc#x&@3-ezm+sfh3w+h#y9-qog(jk$i+c5)rgk=b2fzh'
