9611e6bea21632fbb6d24f586c0668b61fa7d0d3
James Cruddas <cruddasj@informed.com>
2018-04-30 15:54:31 +0100

    Add best effort paypal integration

--- application/views.py
+++ application/views.py
@@ -21,7 +21,6 @@ from .serializers import CardPaymentRequestSerializer, PaypalPaymentRequestSeria
 from .utilities import Utilities
 from lxml import etree
 
-
 # initiate logging
 log = logging.getLogger('django.server')
 
@@ -34,6 +33,31 @@ WORLDPAY_XML_USERNAME = settings.WORLDPAY_XML_USERNAME
 WORLDPAY_XML_PASSWORD = settings.WORLDPAY_XML_PASSWORD
 
 
+@api_view(['PUT'])
+def change_api_key(request):
+    """
+    Function for updating the Worldpay API key used by the payment gateway API
+    :param request: a json request object detailing the new API key to be used
+    :return: a success or failure indicator as to whether the Worldpay API key has been updated
+    """
+    # Change API Key, this is the exact same as the Notify version
+    try:
+        mapped_json_request = Utilities.convert_json_to_python_object(request.data)
+        serializer = ApiKeySerializer(data=mapped_json_request)
+        if serializer.is_valid():
+            # API key set
+            global MERCHANT_CODE
+            MERCHANT_CODE = mapped_json_request['api_key']
+            return JsonResponse({"message": "Api key successfully updated"}, status=200)
+        err = __format_error(serializer.errors)
+        log.error("Django serialization error: " + err[0] + err[1])
+        return JsonResponse({"message": err[0] + err[1], "error": "Bad Request", }, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as ex:
+        exception_data = traceback.format_exc().splitlines()
+        exception_array = [exception_data[-3:]]
+        log.error(exception_array)
+        return JsonResponse(ex.__dict__, status=500)
+
 
 @api_view(['GET'])
 def get_payment(request, payment_id):
@@ -43,7 +67,7 @@ def get_payment(request, payment_id):
     :param payment_id: the order code of the previously issued payment
     :return: a json representation of a previously lodged payment
     """
-    payload = __build_order_query_request(payment_id)
+    payload = __build_get_payment_request(payment_id)
     headers = {"content-type": "text/xml"}
     response = requests.post(WORLDPAY_PAYMENT_ENDPOINT, data=payload, headers=headers,
                              auth=(WORLDPAY_XML_USERNAME, WORLDPAY_XML_PASSWORD))
@@ -86,9 +110,30 @@ def get_payment(request, payment_id):
             "creationDate": payment_date_combined.isoformat(),
             "lastEvent": payment_service_result_reply.get('orderStatus').get('payment').get('lastEvent'),
             "amount": payment_service_result_reply.get('orderStatus').get('payment').get('amount').get('@value'),
-            "currencyCode": payment_service_result_reply.get('orderStatus').get('payment').get('amount').get('@currencyCode')
+            "currencyCode": payment_service_result_reply.get('orderStatus').get('payment').get('amount').get(
+                '@currencyCode')
         }, status=200
     )
+
+
+def __build_get_payment_request(payment_id):
+    """
+    Helper method for creating a request object that can be consumed by the Worldpay XML API to get the latest status of an order.
+    :param payment_id the order code for a payment
+    """
+    payment_service_root = etree.Element('paymentService', version='1.4', merchantCode=str(MERCHANT_CODE))
+    inquiry = etree.SubElement(payment_service_root, 'inquiry')
+    etree.SubElement(inquiry, 'orderInquiry', orderCode=str(payment_id))
+
+    # Append DTD heading to request
+    xml_string = etree.tostring(
+        payment_service_root.getroottree(),
+        xml_declaration=True,
+        encoding='UTF-8',
+        doctype='<!DOCTYPE paymentService PUBLIC "-//Worldpay//DTD Worldpay PaymentService v1//EN" "http://dtd.worldpay.com/paymentService_v1.dtd">'
+    )
+
+    return xml_string
 
 
 @api_view(['POST'])
@@ -125,55 +170,6 @@ def make_card_payment(request):
         return JsonResponse(ex.__dict__, status=500)
 
 
-@api_view(['POST'])
-def make_paypal_payment(request):
-    """
-    Function for making a new Paypal payment via the WorldPay payment processing platform
-    :param request: a json object inclusive of details for a paypal payment to be taken
-    :return: a response inclusive of any URLs that a user should be redirected to (i.e. WorldPay paypal UIs) for making
-    a Paypal payment
-    """
-    mapped_json_request = Utilities.convert_json_to_python_object(request.data)
-    try:
-        serializer = PaypalPaymentRequestSerializer(data=mapped_json_request)
-        if serializer.is_valid():
-            return __create_worldpay_paypal_order_request(serializer.data)
-        err = __format_error(serializer.errors)
-        log.error("Django serialization error: " + err[0] + err[1])
-        return JsonResponse({"message": err[0] + err[1], "error": "Bad Request"}, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as ex:
-        exception_data = traceback.format_exc().splitlines()
-        exception_array = [exception_data[-3:]]
-        log.error(exception_array)
-        return JsonResponse(ex.__dict__, status=500)
-
-
-@api_view(['PUT'])
-def change_api_key(request):
-    """
-    Function for updating the Worldpay API key used by the payment gateway API
-    :param request: a json request object detailing the new API key to be used
-    :return: a success or failure indicator as to whether the Worldpay API key has been updated
-    """
-    # Change API Key, this is the exact same as the Notify version
-    try:
-        mapped_json_request = Utilities.convert_json_to_python_object(request.data)
-        serializer = ApiKeySerializer(data=mapped_json_request)
-        if serializer.is_valid():
-            # API key set
-            global MERCHANT_CODE
-            MERCHANT_CODE = mapped_json_request['api_key']
-            return JsonResponse({"message": "Api key successfully updated"}, status=200)
-        err = __format_error(serializer.errors)
-        log.error("Django serialization error: " + err[0] + err[1])
-        return JsonResponse({"message": err[0] + err[1], "error": "Bad Request", }, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as ex:
-        exception_data = traceback.format_exc().splitlines()
-        exception_array = [exception_data[-3:]]
-        log.error(exception_array)
-        return JsonResponse(ex.__dict__, status=500)
-
-
 def __create_worldpay_card_order_request(card_payment_request):
     """
     Helper method for creating a request object that can be consumed by the Worldpay API to take a card payment
@@ -182,7 +178,8 @@ def __create_worldpay_card_order_request(card_payment_request):
     """
     payload = __build_worldpay_card_payment_xml(card_payment_request)
     headers = {"content-type": "text/xml"}
-    response = requests.post(WORLDPAY_PAYMENT_ENDPOINT, data=payload, headers=headers, auth=(WORLDPAY_XML_USERNAME, WORLDPAY_XML_PASSWORD))
+    response = requests.post(WORLDPAY_PAYMENT_ENDPOINT, data=payload, headers=headers,
+                             auth=(WORLDPAY_XML_USERNAME, WORLDPAY_XML_PASSWORD))
     dictionary = xmltodict.parse(response.text)
 
     payment_service_result = dictionary.get('paymentService')
@@ -248,56 +245,107 @@ def __build_worldpay_card_payment_xml(card_payment_request):
     return xml_string
 
 
-def __build_order_query_request(payment_id):
+@api_view(['POST'])
+def make_paypal_payment(request):
     """
-    Helper method for creating a request object that can be consumed by the Worldpay XML API to get the latest status of an order.
-    :param payment_id the order code for a payment
+    Function for making a new Paypal payment via the WorldPay payment processing platform
+    :param request: a json object inclusive of details for a paypal payment to be taken
+    :return: a response inclusive of any URLs that a user should be redirected to (i.e. WorldPay paypal UIs) for making
+    a Paypal payment
     """
-    payment_service_root = etree.Element('paymentService', version='1.4', merchantCode=str(MERCHANT_CODE))
-    inquiry = etree.SubElement(payment_service_root, 'inquiry')
-    etree.SubElement(inquiry, 'orderInquiry', orderCode=str(payment_id))
+    mapped_json_request = Utilities.convert_json_to_python_object(request.data)
+
+    try:
+        serializer = PaypalPaymentRequestSerializer(data=mapped_json_request)
+        if serializer.is_valid():
+            return __create_worldpay_paypal_order_request(mapped_json_request)
+        err = __format_error(serializer.errors)
+        log.error("Django serialization error: " + err[0] + err[1])
+        return JsonResponse({"message": err[0] + err[1], "error": "Bad Request"}, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as ex:
+        exception_data = traceback.format_exc().splitlines()
+        exception_array = [exception_data[-3:]]
+        log.error(exception_array)
+        return JsonResponse(ex.__dict__, status=500)
+
+
+def __create_worldpay_paypal_order_request(paypal_payment_request):
+    """
+    Helper method for creating a request object that can be consumed by the Worldpay XML API to take a paypal payment
+    :param paypal_payment_request: a request object sent to the payment gateway API containing information for taking a
+    Paypal payment via Worldpay
+    """
+    payload = __build_worldpay_paypal_payment_xml(paypal_payment_request)
+    headers = {"content-type": "text/xml"}
+    response = requests.post(WORLDPAY_PAYMENT_ENDPOINT, data=payload, headers=headers,
+                             auth=(WORLDPAY_XML_USERNAME, WORLDPAY_XML_PASSWORD))
+    dictionary = xmltodict.parse(response.text)
+
+    payment_service_result = dictionary.get('paymentService')
+    payment_service_result_reply = payment_service_result.get('reply')
+
+    if 'error' in payment_service_result_reply.get('orderStatus'):
+        return JsonResponse(
+            {
+                "message": 'Internal Server error',
+                "error": payment_service_result_reply.get('orderStatus').get('error').get('#text')
+            }, status=500
+        )
+    else:
+        return JsonResponse(
+            {
+                "amount": payment_service_result_reply.get('orderStatus').get('payment').get('amount').get('@value'),
+                "customerOrderCode": payment_service_result_reply.get('orderStatus').get('@orderCode'),
+                "paymentMethod": payment_service_result_reply.get('orderStatus').get('payment').get('paymentMethod'),
+                "currencyCode": payment_service_result_reply.get('orderStatus').get('payment').get('amount').get(
+                    '@currencyCode'),
+                "redirectUrl": payment_service_result_reply.get('orderStatus').get('payment').get('redirectUrl'),
+            }
+            , status=201)
+
+
+def __build_worldpay_paypal_payment_xml(paypal_payment_request):
+    """
+    Helper method for creating an XML request object for dispatch to the Worldpay API
+    :param paypal_payment_request: an inbound paypal payment request received by the payment gateway API
+    :return: a structured XML request to be POSTed to Worldpay
+    """
+    payment_service_root = etree.Element('paymentService', version='1.2', merchantCode=str(MERCHANT_CODE))
+    submit = etree.SubElement(payment_service_root, 'submit')
+    order = etree.SubElement(
+        submit, 'order', orderCode=str(paypal_payment_request['customer_order_code']),
+        shopperLanguageCode=str(paypal_payment_request['shopperCountryCode'])
+    )
+
+    description = etree.SubElement(order, 'description')
+    description.text = paypal_payment_request['order_description']
+
+    etree.SubElement(
+        order, 'amount', currencyCode=str(paypal_payment_request['currency_code']),
+        exponent='2', value=str(paypal_payment_request['amount'])
+    )
+
+    payment_details_element = etree.SubElement(order, 'paymentDetails')
+    paypal_express = etree.SubElement(payment_details_element, 'PAYPAL-EXPRESS')
+
+    success_url = etree.SubElement(paypal_express, 'successURL')
+    success_url.text = str(paypal_payment_request['success_url'])
+
+    failure_url = etree.SubElement(paypal_express, 'failureURL')
+    failure_url.text = str(paypal_payment_request['failure_url'])
+
+    cancel_url = etree.SubElement(paypal_express, 'cancelURL')
+    cancel_url.text = str(paypal_payment_request['cancellation_url'])
 
     # Append DTD heading to request
     xml_string = etree.tostring(
         payment_service_root.getroottree(),
         xml_declaration=True,
         encoding='UTF-8',
-        doctype='<!DOCTYPE paymentService PUBLIC "-//Worldpay//DTD Worldpay PaymentService v1//EN" "http://dtd.worldpay.com/paymentService_v1.dtd">'
+        doctype='<!DOCTYPE paymentService PUBLIC "-//Bibit//DTD Bibit PaymentService v1//EN" "http://dtd.bibit.com/paymentService_v1.dtd">'
     )
 
     return xml_string
-
-
-def __create_worldpay_paypal_order_request(paypal_payment_request):
-    """
-    Helper method for creating a request object that can be consumed by the Worldpay API to take a paypal payment
-    :param paypal_payment_request: a request object sent to the payment gateway API containing information for taking a
-    Paypal payment via Worldpay
-    :return: a json request that can be consumed by the Worldpay API for making a Paypal payment
-    """
-    payload = {
-        "paymentMethod": {
-            "type": "APM",
-            "apmName": "paypal",
-            "shopperCountryCode": paypal_payment_request["shopper_country_code"]
-        },
-        "amount": paypal_payment_request["amount"],
-        "currencyCode": paypal_payment_request["currency_code"],
-        "orderDescription": paypal_payment_request["order_description"],
-        "customerOrderCode": paypal_payment_request["customer_order_code"],
-        "successUrl": paypal_payment_request["success_url"],
-        "pendingUrl": paypal_payment_request["pending_url"],
-        "failureUrl": paypal_payment_request["failure_url"],
-        "cancelUrl": paypal_payment_request["cancellation_url"],
-    }
-
-    headers = {"content-type": "application/json", "Authorization": MERCHANT_CODE}
-    response = requests.post('https://api.worldpay.com/v1/orders/', data=json.dumps(payload), headers=headers)
-
-    if response.status_code == 200:
-        return JsonResponse(json.loads(response.text), status=201)
-    else:
-        return JsonResponse(json.loads(response.text), status=response.status_code)
 
 
 def __format_error(ex):
